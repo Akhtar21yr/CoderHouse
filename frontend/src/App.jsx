907fbff8ab8b2d6acc7a8dd79b9cf760c494f2b4
akhtar21yr <akhtar.decy@gmail.com>
2024-12-02 17:46:37 +0530

    created : register & login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Navigation from './components/shared/Navigation/Navigation'
+import Register from './pages/Register/Register'
+import Login from './pages/Login/Login'
 
 
 
@@ -11,6 +13,8 @@ const App = () => {
     <Navigation/>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register/>} />
+        <Route path='/login' element={<Login/>} />
       </Routes>
     </BrowserRouter>
   )
