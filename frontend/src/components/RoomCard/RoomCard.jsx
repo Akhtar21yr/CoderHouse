30c2993832f347866c28ddee68eb9c14b3893c58
akhtar21yr <akhtar.decy@gmail.com>
2024-12-09 17:48:08 +0530

    update room route

--- frontend/src/components/RoomCard/RoomCard.jsx
+++ frontend/src/components/RoomCard/RoomCard.jsx
@@ -6,7 +6,7 @@ import {useNavigate} from 'react-router-dom'
 const RoomCard = ({ room }) => {
     const navigate = useNavigate()
     return (
-        <div onClick={()=>navigate(`/rooms/${room.id}`)} className={styles.card}>
+        <div onClick={()=>navigate(`/room/${room.id}`)} className={styles.card}>
             <h3 className={styles.topic}>{room.topic}</h3>
             <div className={`${styles.speakers} ${room.speakers.length === 1 ? styles.singleSpeaker : ''}`}>
                 <div className={styles.avatars}>
